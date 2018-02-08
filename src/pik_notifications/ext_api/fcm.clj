42b73e1702a5640ca28aa184afb9850536cc01c5
TM <tm@macadmin.ru>
2018-02-08 13:00:36 +0300

    add state to fcm ns;

--- src/pik_notifications/ext_api/fcm.clj
+++ src/pik_notifications/ext_api/fcm.clj
@@ -1,23 +1,20 @@
 (ns pik-notifications.ext-api.fcm
-  (:require [pik-notifications.config :refer [settings]]
+  (:require [mount.core :refer [defstate]]
+            [pik-notifications.config :refer [settings]]
             [pik-notifications.ext-api.core :as c]
             [pik-notifications.db.core :as db]))
 
 
-(def api-url "https://fcm.googleapis.com/fcm/send")
-
-(defn api-key []
-  (get settings :google-api-key))
-
+(defstate api
+  :start
+  {:url "https://fcm.googleapis.com/fcm/send"
+   :default-params {:insecure? true
+                    :headers {"Authorization" (str "key=" (:google-api-key settings))
+                              "Content-Type" "application/json"}}})
 
 (def max-ids-in-notification 1000)
 
 (def dry-run false)
-
-(defn default-params []
-  {:insecure? true
-   :headers {"Authorization" (str "key=" (api-key))
-             "Content-Type" "application/json"}})
 
 
 (defn- make-notification [ids text]
@@ -28,7 +25,7 @@
 
 
 (defn- make-request [notification]
-  (assoc (default-params) :body (c/to-json notification)))
+  (assoc (:default-params api) :body (c/to-json notification)))
 
 
 (defn- process-result [[k v]]
@@ -56,7 +53,7 @@
   (let [filtered-ids (db/filter-ids ids)]
     (when-not (empty? filtered-ids)
       (let [req (make-request (make-notification filtered-ids text))
-            {:keys [status body error] :as resp} (c/post-safely api-url req)]
+            {:keys [status body error] :as resp} (c/post-safely (:url api) req)]
         (cond-> {:status status}
           error (assoc :error error)
           (= 200 status) (assoc :result (process-resp-body! filtered-ids body)))))))
@@ -71,7 +68,21 @@
        (reduce +)
        (assoc {} :success)))
 
+
 ;(def ids ["001" "APA91bGms1iKIPdKURo7CAi1bGx4kZ_1EB-8m950An3LPDkvMRkxRKofz7yvuZGvzrFpDl6hjOz5zjNbMA1ue2xfBknnGU-dwkUVC74A-rYHIOdydX2PgDZ4IPdT2C_Gjl6eHq_5c356" "APA91bHj6HU0Zvftt8bGmWo1zf95s0M0KT3K4_6U55LN9tByUM_J3r3yqW41ehelDMb2cRZ0iiR0xcvo3g1nS-rKTWGi8PK4Ln8K9zRVIBtB_HIp6iZFlCqtpOP9P7H5wp6e_4cSXAL5" "234" "456"])
+;
+;(def filtered-ids (db/filter-ids ids))
+;
+;(def req (make-request (make-notification filtered-ids "test 1")))
+;
+;(identity req)
+;
+;(:url api)
+;
+;(def resp (c/post-safely (:url api) req))
+;(identity resp)
+
+;(send-notification ids "test 1")
 
 ;(def r (send-notification ids "t-0"))
 ;(identity r)
